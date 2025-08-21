b9f90a181178e1ab959fc7d49ca302cfc1387e36
BorgesRodrigo <borgesrodrigo2024@outlook.com>
2025-08-20 22:40:09 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -4,7 +4,6 @@ import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
